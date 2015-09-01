6ecb51ed3f2fcb965aff8e3f81489242ebf7c178
Gregory Shilin <gshilin@gmail.com>
2015-09-01 14:45:36 +0300

    routing

--- sendgrid.go
+++ sendgrid.go
@@ -1,15 +1,25 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"os"
+
+	"github.com/julienschmidt/httprouter"
 )
 
 func main() {
+	r := httprouter.New()
+	r.POST("/api/sendgrid_event/", ProcessEvent)
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 	}
 
-	http.ListenAndServe(":" + port, nil)
+	http.ListenAndServe(":" + port, r)
+}
+
+func ProcessEvent(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	fmt.Fprintln(rw, "event processed")
 }
